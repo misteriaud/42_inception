a5c4e6f0cd92407e910683530ca3d1ec71ac8f39
Maxime inside VM <mriaud@student.42.fr>
2023-03-20 13:09:47 -0400

    <fix>: using wp_cli

--- Makefile
+++ Makefile
@@ -1,18 +1,18 @@
-WORDPRESS = ${HOME}/data/wordpress_static/wordpress
 DATA_DIR = $(addprefix ${HOME}/data/, wordpress_static wordpress_db)
 # DATA_DIR = $(HOME)/data
 
-all: $(DATA_DIR) $(WORDPRESS)
+all: up
+
+up: $(DATA_DIR) $(WORDPRESS)
 	docker compose -f ./srcs/docker-compose.yml up --build
+
+down:
+	docker compose -f ./srcs/docker-compose.yml down
 
 $(DATA_DIR):
 	mkdir -p $@
 
-$(WORDPRESS):
-	wget http://wordpress.org/latest.tar.gz -O /tmp/latest.tar.gz
-	tar -xzf /tmp/latest.tar.gz -C $(HOME)/data/wordpress_static
-
-fclean:
+fclean: down
 	rm -rf $(DATA_DIR)
 
-.PHONY: set_data_dir
+.PHONY: up down
---
+++ srcs/php_image/tools/php_launcher.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "php_launcher running";
+ls /usr/local/bin/wp;
+
+if [ ! -d "/usr/share/wordpress" ]; then
+	echo "wordpress missing, downloading wordpress";
+	wp core download --path=/usr/share/wordpress;
+fi
+
+	# && wp db create --path='/usr/share/wordpress' \
+if [ ! -f "/usr/share/wordpress/wp-config.php" ]; then
+	echo "wordpress config file missing, setting up config file";
+	wp config create	--allow-root \
+						--dbname=$DB_WORDPRESS_NAME \
+						--dbuser=$DB_WORDPRESS_USERNAME \
+						--dbpass=$DB_WORDPRESS_USERPASS \
+						--dbhost=mariadb:3306 \
+						--path='/usr/share/wordpress' \
+	&& echo "wp core install" \
+	&& wp core install	--url=$DOMAIN_NAME \
+						--title="$WP_TITLE" \
+						--admin_user=$WP_ADMIN_NAME \
+						--admin_password=$WP_ADMIN_PASS \
+						--admin_email=$WP_ADMIN_EMAIL \
+						--path='/usr/share/wordpress' \
+	&& echo "wp user create" \
+	&& wp user create	$WP_USER_NAME $WP_USER_EMAIL \
+						--user_pass=$WP_USER_PASS \
+						--role=author \
+						--path='/usr/share/wordpress';
+else
+	echo "already configured";
+fi
+
+exec php-fpm8;
